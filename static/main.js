b3f0f363cd99232f27a87be4918c737b8f4893b9
tom <cyt046@gmail.com>
2015-06-24 19:51:29 -0400

    added reverse-geocoding

--- static/main.js
+++ static/main.js
@@ -1,5 +1,7 @@
 google.setOnLoadCallback(main);
 
+var GOOGLE_REV_GEOCODE_URL = 'https://maps.googleapis.com/maps/api/geocode/json'
+
 
 function toReadableDate(date) {
   var hours = date.getHours().toString(),
@@ -11,11 +13,13 @@ function toReadableDate(date) {
 }
 
 
-function createToolTip(brk) {
+function createToolTip(brk, loc, breakId) {
   var start = new Date(brk.start);
-  return "<div style='padding:5px 5px 5px 5px; width: 250px;'>"+
+  var tooltipStyle = 'padding:5px 5px 5px 5px; width: 220px; height: 100px;'
+  return "<div class='break-display' id='"+breakId+"' style='"+tooltipStyle+"'>"+
             "<b>How long</b>:&nbsp;"+brk.duration+"<br/>"+
             "<b>When</b>:&nbsp;"+toReadableDate(start)+"<br/>"+
+            "<b>Where</b>:&nbsp;"+loc+"<br/>"+ 
           "</div>";
 }
 
@@ -28,45 +32,103 @@ function makeChart(container, raceId, numRacers) {
   dataTable.addColumn({ type: 'string', role: 'tooltip', p: {html: true}});
   dataTable.addColumn({ type: 'date', id: 'Start' });
   dataTable.addColumn({ type: 'date', id: 'End' });
-  var fetched = 0;
-  var geocoder = new google.maps.Geocoder();
+  // cache of breaks
+  var breakCache = {}
+  // cache of addresses (reverse-geocoding result)
+  var addrCache = {}
+  var numFetched = 0;
+  var geocode= function(lat, lng, callback) {
+    /*
+     * reverse geocode
+     */
+    var latlng = lat+","+lng;
+    // check cache -- avoid unecessary API call
+    if (addrCache[latlng] != undefined) {
+      var cached = addrCache[latlng];
+      var results = cached[0];
+      var ok = cached[1];
+      callback(results, ok);
+    } else {
+      var url = GOOGLE_REV_GEOCODE_URL+"?latlng="+latlng+"&key="+api_key;
+      return $.getJSON(url, function(resp) {
+        var ok = (resp.results != undefined && resp.results.length > 0);
+        // save to cache
+        addrCache[latlng] = [resp.results, ok];
+        callback(resp.results, ok);
+        if (!ok) {
+          console.error(resp);
+        }
+      });
+    }
+  }
+  var handleMouseover = function(e) {
+    /*
+     * deal with mouseover here
+     */
+    var selected = $('.break-display')[0];
+    var breakId = selected.id;
+    var brk = breakCache[breakId]; 
+    var racerName = breakId.split('*')[0].replace("-", " ");
+    var lat = brk.lat,
+        lng = brk.lng;
+    geocode(lat, lng, function(results, ok) {
+      if (ok) {
+        var loc = results[0].formatted_address;
+        var newToolTip = createToolTip(brk, loc, breakId);
+        $(selected).html(newToolTip);
+      }
+    });
+  };
+
+  var updateChart = function() {
+    /*
+     * closure used to update the chart
+     * called when ALL data of a racer are loaded
+     * -- including reverse-geocoding data
+     */
+    numFetched += 1;
+    // update progress bar
+    var percentDone = (numFetched/numRacers*100).toFixed(2)+"%";
+    var visualProgress = "Loading data - "+percentDone;
+    $('#progress-display').text(visualProgress);
+    $('#progress-bar').width(percentDone);
+
+    // add data to chart
+    var options = {
+      // magic
+      height: numRacers * 50 + 30,
+      timeline: {singleColor: '#8d8'},
+      tooltip: {isHtml: true}
+    };
+    $('.google-visualization-tooltip').remove();
+    google.visualization.events.addListener(chart, 'onmouseover', handleMouseover);
+    chart.draw(dataTable, options);
+    // done, remove progress bar
+    if (numFetched == numRacers) {
+      $('#progress').hide();
+    }
+  };
 
   return {
     showBreaks: function(racerName) {
       /*
        * given name of the racer fetch break data from the server
        */
-      $.getJSON("/breaks/"+raceId+"?name="+racerName, function(feed) {
+      $.getJSON("/breaks/"+raceId+"?name="+racerName, function(feed) { 
         feed.breaks.forEach(function(brk) {
-          // TODO add break location
           var start = new Date(brk.start),
-            end = new Date(brk.end);
-          //var tooltip = racerName+" stopped for "+brk.duration+" "+toReadableDate(start);
-          var tooltip = createToolTip(brk);
+              end = new Date(brk.end);
+          var loc = brk.lat+", "+brk.lng;
+          // there's only one spot/break for any racer at a given location
+          // so the pair of racer name and a location should be unique,
+          // giving a valid id for a break
+          var breakId = racerName.replace(" ", "-")+"*"+brk.lat+","+brk.lng;
+          var tooltip = createToolTip(brk, loc, breakId);
+          breakCache[breakId] = brk;
           dataTable.addRow([racerName, "", tooltip, start, end]);
         });
 
-        fetched += 1;
-        // update progress bar
-        var percentDone = (fetched/numRacers*100).toFixed(2)+"%";
-        var visualProgress = "Loading data - "+percentDone;
-        $('#progress-display').text(visualProgress);
-        $('#progress-bar').width(percentDone);
-
-        // add data to chart
-        var options = {
-          // magic
-          height: numRacers * 50 + 30,
-          timeline: {singleColor: '#8d8'},
-          tooltip: {isHtml: true}
-        };
-        $('.google-visualization-tooltip').remove();
-        chart.draw(dataTable, options);
-
-        // done, remove progress bar
-        if (fetched == numRacers) {
-          $('#progress').hide();
-        }
+        updateChart();
       }); 
     },
   };
--- web.py
+++ web.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify, Response, render_template
 from trackleaders import get_breaks, get_racer_id, get_racers, get_race_name
+from config import GOOGLE_API_KEY
 
 app = Flask(__name__)
 
@@ -10,7 +11,8 @@ BAD_REQUEST = Response(status='405')
 def home(race_id):
     return render_template('index.html',
             race_id=race_id,
-            race_name=get_race_name(race_id))
+            race_name=get_race_name(race_id),
+            api_key=GOOGLE_API_KEY)
 
 
 @app.route('/breaks/<race_id>')
